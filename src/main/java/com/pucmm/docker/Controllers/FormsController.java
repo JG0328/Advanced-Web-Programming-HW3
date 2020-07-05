48c610fbe21f95774a7b273bb55aad97c9c67685
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-07-05 03:20:34 -0400

    Create survey form

--- src/main/java/com/pucmm/docker/Controllers/FormsController.java
+++ src/main/java/com/pucmm/docker/Controllers/FormsController.java
@@ -3,14 +3,22 @@ package com.pucmm.docker.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path = "/barcamp")
+@RequestMapping(path = "/")
 public class FormsController {
-    @GetMapping
-    @ResponseBody
-    public String test() {
-        return "Hello World";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView indexClients() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("clients", "");
+        model.setViewName("form");
+        return model;
     }
+
 }
+
+
